848dbbfabe35549e9a65c10172b9c18cc27f41d8
fernandosuarezhmo <fernandosuarezhmo@gmail.com>
2023-06-29 18:39:03 -0700

    CounterWithCustomHook Updated

--- src/02-useState/CounterWithCustomHook.jsx
+++ src/02-useState/CounterWithCustomHook.jsx
@@ -6,12 +6,12 @@ export const CounterWithCustomHook = () => {
 
   return (
     <>
-      <h1>Counter With Hook: { counter }</h1>
+      <h1>Counter with Custom Hook: { counter }</h1>
       <hr />
 
-      <button onClick={ (value) => increment(2) }className="btn btn-primary">+1</button>
+      <button onClick={ (value) => decrement(1) }className="btn btn-primary">-1</button>
       <button onClick={ reset } className="btn btn-primary">Reset</button>
-      <button onClick={ (value) => decrement(2) }className="btn btn-primary">-1</button>
+      <button onClick={ (value) => increment(1) }className="btn btn-primary">+1</button>
 
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Padre } from './Padre';
+//import { Padre } from './Padre';
 //import { CallbackHook } from './06-memos/CallbackHook';
 //import { MemoHook } from './06-memos/MemoHook';
-import { Memorize } from './06-memos/Memorize';
+//import { Memorize } from './06-memos/Memorize';
 //import { Layout } from './05-useLayoutEffect/Layout';
 //import { FocusScreen } from './04-useRef/FocusScreen';
 //import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 //import { FormWithCustomHook } from './01-useEffect/FormWithCustomHook';
 //import { SimpleForm } from './01-useEffect/SimpleForm';
-//import { CounterApp } from './01-useState/CounterApp';
-//import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+//import { CounterApp } from './02-useState/CounterApp';
+import { CounterWithCustomHook } from './02-useState/CounterWithCustomHook';
 //import { HooksApp } from './HooksApp';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   //<React.StrictMode>
-    <Padre />
+    <CounterWithCustomHook />
   //</React.StrictMode>,
 )
